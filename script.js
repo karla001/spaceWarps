0e0f87847a3d15a3ed28ce6ede238864d07e4bbd
Karla Gilsalazar <karla.gilsalazar@gmail.com>
2016-10-11 14:35:29 -0700

    basic game logic completed

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ var player1Current = 0;
 var player2Current = 0;
 
 var  playerTurn= player1;
+var moveme;
 
 //calling the draw function o set up my game
 draw();
@@ -39,7 +40,7 @@ function initializeModels () {
   //return to starting Game model
   diceval = null;
   win = false;
-};
+};//---------func is working----------
 
 //this func is called when the dice is rolled
 //and player position is updated/ turn is complete
@@ -67,26 +68,32 @@ function updateTurn(){
 //This func is called in rollDice func
 //after diceval has been generated
 function playerPosition(){
-  if (playerTurn === player1) {
-    //retreive index value and add
-    player1Current = player1Current + diceval
-    // player1Current.text(player1)
-    //new diceval to index # creating a new
-    //board[i] position
-  }else if (playerTurn === player2) {
-    //retreive index value and ad
-    player2Current = player2Current + diceval
-    // player2Current.text(player2)
-    //new diceval to index # creating a new
-    //board[i] position
-  }
-  //for statement did not work here!
-  if(player2Current>28 || player1Current>28) {
-    winCheck();
-  }
-  // clearCells();//TESTING
-  clearCells()
-  renderjsBoard();//originally first thing in draw!!
+  setTimeout(function(){
+    //this is showing the person who's turn it just became
+    if (playerTurn === player2) {
+      //retreive index value and add
+      player1Current = player1Current + diceval
+      // player1Current.text(player1)
+      //new diceval to index # creating a new
+      //board[i] position
+    }else if (playerTurn === player1) {
+      //retreive index value and ad
+      player2Current = player2Current + diceval
+      // player2Current.text(player2)
+      //new diceval to index # creating a new
+      //board[i] position
+    }
+    //for statement did not work here!
+    if(player2Current>28 || player1Current>28) {
+      winCheck();
+    }
+    // clearCells-func is being called properly
+    clearCells()
+    renderjsBoard();//originally first thing in draw!!
+     //Warp is working!
+    Warp(player1Current);
+    Warp(player2Current);
+  }, 600);
 }; /*------------------func is Working!!----------------------*/
 
 
@@ -170,7 +177,7 @@ function changeDiceFace(){
       "background-image": 'url(assets/d4.png)'
     });
   }
-};
+};//---------------func is working-----------
 
 function resetGame(){
   //reset all values to starting values
@@ -184,7 +191,7 @@ function resetGame(){
     draw();
     console.log('Game has been reset');
   });
-}; /*------------------ready for testing----------------------*/
+}; /*------------------func is working----------------------*/
 
 //check for win
 function winCheck(){
@@ -199,22 +206,67 @@ function renderjsBoard(){
   //this loop should stop when cells no longer available
   //instead it is breaking"canot set texcontent of undefined"
   for(i = 0; i < cells.length; i++){
-    if (player1Current > 28 || player2Current > 28) {
+      if (player1Current === player2Current) {
+      cells[player1Current].textContent = player1 + player2;
+    }else if (player1Current > 28 || player2Current > 28) {
       draw();
     }else{
       cells[player1Current].textContent = player1
       cells[player2Current].textContent = player2
     }
   }
-};
+};//-----------------func is working---------
 //--------------------------add the Warps!!!----------------------
-// function Warp(){
-//   if () {
-
-//   }
-
-// };
-//------------------Clearing cells
+function Warp(current){
+  setTimeout(function () {
+    switch(current) {
+      //Warps-sending players backwards
+      case 6:
+          moveme = 1
+          alert('Warp has been activated');
+          break;
+      case 11:
+          moveme = 4
+          alert('Warp has been activated');
+          break;
+      case 16:
+          moveme = 7
+          alert('Warp has been activated');
+          break;
+      case 21:
+          moveme = 13
+          alert('Warp has been activated');
+          break;
+      case 27:
+          moveme = 20
+        alert('Warp has been activated');
+        break;
+      //warps-sending players forward
+      case 5:
+          moveme = 10
+          alert('Warp has been activated');
+          break;
+      case 15:
+          moveme = 18
+          alert('Warp has been activated');
+          break;
+      case 19:
+          moveme = 22
+          alert('Warp has been activated');
+          break;
+      default:
+          moveme = current
+    }
+    if (current === player1Current) {
+     player1Current = moveme
+    }else if (current === player2Current) {
+      player2Current = moveme
+    }
+    clearCells()
+    renderjsBoard();//originally first thing in draw!!
+  }, 500);
+};
+//------------------Clearing cells-Func is working-----------------
 function clearCells(){
   console.log('clearing cells');
   for (var i = 0; i < cells.length; i++) {
